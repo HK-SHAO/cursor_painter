cbc1ff076c71c5ca0e3b3aafdd4ab64b65017071
王家晔 <2063597709@qq.com>
2020-02-19 16:53:44 +0800

    ''

--- painter.py
+++ painter.py
@@ -1,6 +1,6 @@
+from PIL import Image, ImageFilter, ImageGrab
 from pynput.mouse import Button, Controller
 from pynput import keyboard
-from PIL import Image, ImageFilter, ImageGrab
 import time
 
 mouse = Controller()
@@ -8,18 +8,27 @@ p = None
 img = None
 isB = False
 
+
 def click(x, y):
     mouse.position = (x, y)
     mouse.click(Button.left, 1)
 
-def draw(s, pt = p, n = 1, m = '00', r = 1, t = 220, st = 0.01):
+
+def rgb_distance(rgb_1, rgb_2):
+    r1, g1, b1 = rgb_1
+    r2, g2, b2 = rgb_2
+    return ((r1-r2)**2+(g1-g2)**2+(b1-b2)**2)**0.5
+
+
+def draw(s, pt=p, n=1, m='00', r=1, t=220, st=0.01):
     global img
     img = Image.open(s)
     img = img.resize((int(img.width*n), int(img.height*n)))
     pt = p
 
     if m[0] == '0':
-        img = img.filter(ImageFilter.SMOOTH).filter(ImageFilter.CONTOUR).convert('L')
+        img = img.filter(ImageFilter.SMOOTH).filter(
+            ImageFilter.CONTOUR).convert('L')
     else:
         img = img.convert('L')
 
@@ -33,54 +42,59 @@ def draw(s, pt = p, n = 1, m = '00', r = 1, t = 220, st = 0.01):
 
             pix = pixels[x, y]
             if r*pix < r*t:
-                if m[1] == '0' or (x%2==0 and y%2==0):
+                if m[1] == '0' or (x % 2 == 0 and y % 2 == 0):
                     click(pt[0]+x, pt[1]+y)
                     time.sleep(st)
 
-def color(s, n = 1, st = 0.01):
-    global img
-    img = Image.open(s)
-    img = img.resize((int(img.width*n), int(img.height*n))).convert('RGB')
 
+def color(s, n=1, st=0.01):
+    global img
     colors, pos = [], []
-    print("将光标移动到颜色按钮，回车添加，输入s并回车从当前光标位置开始绘制，输入q并回车退出")
+    print("先将光标移动到画板上回车，再将光标移动到颜色按钮回车\n最后输入s并回车从当前光标位置开始绘制，输入q并回车退出")
     while True:
         input_str = input()
         if input_str == 's':
-            mp = mouse.position
             break
         elif input_str == 'q':
             return
         mp = mouse.position
+        if len(pos) == 0:
+            mp = (mp[0]-2, mp[1]-2)
         c = ImageGrab.grab().getpixel(mp)
         pos.append(mp)
         colors.append(c)
         print('{} {}'.format(c, mp))
 
+    mp = mouse.position
+    img = Image.open(s)
+    img = img.resize((int(img.width*n), int(img.height*n))).convert('RGB')
     pixels = img.load()
-    for x in range(img.width):
-        for y in range(img.height):
+    while True:
+        for x in range(img.width):
+            for y in range(img.height):
 
-            if isB:
-                print('已中止')
-                return
+                if isB:
+                    print('已中止')
+                    return
 
-            pix = pixels[x, y]
-            if (pix[0]+pix[1]+pix[2])/3 < 240:
+                pix = pixels[x, y]
                 ds = []
                 for c in colors:
-                    d = ((pix[0]-c[0])**2+(pix[1]-c[1])**2+(pix[2]-c[2])**2)**0.5
-                    ds.append(d)
+                    ds.append(rgb_distance(c, pix))
                 index = ds.index(min(ds))
+                if index != 0:
+                    click(pos[index][0], pos[index][1])
+                    click(mp[0]+x, mp[1]+y)
+                    time.sleep(st)
+        if input('输入s再画一个，回车则结束：') != 's':
+            return
 
-                click(pos[index][0], pos[index][1])
-                click(mp[0]+x, mp[1]+y)
-                time.sleep(st)
 
 def on_release(key):
     if key == keyboard.Key.esc:
         global isB
         isB = True
+
 
 listener = keyboard.Listener(on_release=on_release)
 listener.start()
@@ -113,5 +127,5 @@ while True:
         o = eval(i)
         if o != None:
             print(o)
-    except:
-        print('error')
+    except Exception as e:
+        print(e)
